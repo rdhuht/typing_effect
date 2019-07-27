3fc122655a90b5b718838180d24f705ac05b60c6
Leonardo <45762153+leonardojung2001@users.noreply.github.com>
2019-07-27 08:44:58 -0300

    Add files via upload

---
+++ self_writing.py
@@ -0,0 +1,21 @@
+import time
+import os
+
+#phrase as a string
+def write(phrase):
+   list = [letter for letter in phrase] #create a list with the string
+   strg = "" #the last pair of words
+   pos = 0 #inicial index position
+   
+   for item in list:  
+      strg = strg + list[0 + pos]
+         
+      time.sleep(0.1) #speed
+      os.system("clear")
+      print(strg)
+         
+      pos = pos + 1 #change the letter
+
+if __name__ == "__main__":
+   write("leonardo gostoso do caralho") #type your phrase here
+      
